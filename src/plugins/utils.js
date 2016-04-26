9a74958c047226bab1826bd1b1cdc055103f6898
tmorin <thibault.morin@b-i.com>
2016-04-26 12:35:06 +0200

    remove systemjs support

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -6,10 +6,3 @@ import {compile} from '../idomizer.js';
 export function toStringFunction(html, options) {
     return compile(html, options).toString().replace('function anonymous', 'function');
 }
-
-/**
- * @ignore
- */
-export function toFunction(stringFunction) {
-    return new Function(['i', 'h'], `return (${stringFunction}(i, h));`);
-}
