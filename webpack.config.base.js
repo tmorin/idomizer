ef54c0fb40da23a9d5b5b04fe1e75fd5a82cf944
tmorin <thibault@morin.io>
2017-02-22 10:41:05 +0100

    upgrade webpack dependency

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -4,9 +4,9 @@ var webpack = require('webpack');
 
 module.exports = {
     module: {
-        loaders: [
-            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel'},
-            {test: /\.json/, loader: 'json'}
+        rules: [
+            {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test: /\.json/, loader: 'json-loader'}
         ]
     },
     output: {
