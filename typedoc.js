a42fd1f97dec0042ac6b072d90723bc42a850044
tmorin <thibault@morin.io>
2020-09-20 09:54:52 +0200

    chore: switch to github actions

--- typedoc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    out: './typedoc',
-    name: 'idomizer',
-    readme: './README.md',
-    externalPattern: 'node\\_modules',
-    excludePrivate: true,
-    excludeProtected: true,
-    target: 'ES6'
-};
